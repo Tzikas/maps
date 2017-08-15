3decaa95bb4fc83b7dfd1352a19e909fb872ae1e
Kyle Rothenbaum <krothenbaum@gmail.com>
2017-08-15 12:29:02 -0700

    Updated to Score API Endpoints

--- server/routes/api/score.js
+++ server/routes/api/score.js
@@ -4,19 +4,22 @@ module.exports = (app) => {
 	app.get('/api/score', (req, res, next) => {
 		Score.find()
 			.exec()
-			.then((city) => console.log(city))
+			.then((score) =>
+				console.log('Random Score: ' + score);
+			)
 			.catch((err) => next(err));
 	});
 	
 	app.post('/api/score', function (req, res, next) {
-		const score = new Score({name: req.body.name, score: 0})
+		const score = new Score({name: req.body.name, score: req.body.score});
 		score.save()
 			.then(newScore => {
-				console.log(newScore);
+				console.log('Score saved: ' + newScore);
+				res.status(201).redirect('/');
 			})
 			.catch(err => {
 				console.log(err);
+				next(err);
 			});
 	});
-
 };
