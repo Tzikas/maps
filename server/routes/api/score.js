600cee2f15318a3580a118951e234ad9d1c96049
Kyle Rothenbaum <krothenbaum@gmail.com>
2017-08-15 14:53:01 -0700

    Top Score Endpoint added

--- server/routes/api/score.js
+++ server/routes/api/score.js
@@ -2,13 +2,26 @@ const Score = require('../../models/score');
 
 module.exports = (app) => {
 	app.get('/api/score', (req, res, next) => {
+		console.log('In GET Score api');
 		Score.find()
 			.exec()
-			.then((score) =>
+			.then((score) => {
 				console.log('Random Score: ' + score);
-			)
+			})
 			.catch((err) => next(err));
 	});
+
+	app.get('api/score/top'), (req, res, next) => {
+		Score
+			.find()
+			.sort({score: -1})
+			.limit(10)
+			.exec()
+			.then((scores) => {
+				console.log(scores);
+			})
+			.catch((err) => next(err));
+	}
 	
 	app.post('/api/score', function (req, res, next) {
 		const score = new Score({name: req.body.name, score: req.body.score});
